c1ae6bce70049a51c38bbdfec5a18edc7e53d10f
chul soo <opportunity13@naver.com>
2022-12-19 17:34:21 +0900

    commmit

--- server.js
+++ server.js
@@ -6,8 +6,19 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('yes')
+    res.render('index')
 })
+app.get('/account-login', (req, res) => {
+    res.render('account-login')
+})
+app.get('/wishlists', (req, res) => {
+    
+})
+app.get('/cart', (req, res) => {
+    
+})
+
+
 app.listen(3030, () => {
     console.log('The Server is running...');
 })
