85df04cc2d12a9901cc8ff3c83bf273b6fca6e0c
TaekHyun <=>
2023-01-09 21:34:39 +0900

    finish wishlists cookie

--- public/js/index.js
+++ public/js/index.js
@@ -36,47 +36,52 @@ for(let i = 0; i < arr.length; i++){
 
 var cookies = [];
 
-parseCookie = () => { //익명함수
+parseCookie = (ck) => { //익명함수
 
-    if(document.cookie){
-        let cookie = document.cookie;
-        let i = 0;
-
+    if(document.cookie !== ''){
+        let cookie = ck == undefined ? document.cookie : ck;
+        let array;
+        let on;
         for(str of cookie){ //배열 길이를 지정하지않아 undefined가 들어가는 것을 방지
             if(str == '{'){
-                cookies[i] = '';
+                array = '';
+                on = 1;
             }
-
-            if(str == '}'){
-                cookies[i] += str;
-                i++;
-            }else{
-                cookies[i] += str;
+            if(on){ 
+                if(str == '}'){
+                    array += str;
+                    on = 0;
+                    pushCookie(array);
+                }else{
+                    array += str;
+                }
             }
-        }
-        for(let i = 0; i < cookies.length; i++){
-            cookies[i] = JSON.parse(cookies[i]);
+            
         }
     }
 }
+pushCookie = (ck) => {
+    cookies.push(JSON.parse(ck));
+}
 parseCookie();
-console.log(cookies);
 
 for(let i = 0; i < cookies.length; i++){ //처음들어왔을 때 이전에 저장한 위시리스트가 있을 때 배경색을 바꾼다.
+    console.log('shit');
     let divbox = document.getElementsByClassName(`shoes mos${Number(cookies[i].index)}`);
     arr[cookies[i].index].isOn = cookies[i].isOn;
     arr[cookies[i].index].btn_color = cookies[i].btn_color;
     divbox[0].children[3].style.backgroundColor = arr[cookies[i].index].btn_color;
 }
-function setCookie(p, mxAge){
-    console.log(JSON.stringify(arr[p]));
-    document.cookie = `index${p}=`+JSON.stringify(arr[p]) + ";" + 'max-age='+mxAge+';';
-    parseCookie()
+
+function setCookie(p, mxAge){ //쿠키세팅
+    let cookie = `index${p}=`+JSON.stringify(arr[p]) + ";" + 'max-age='+mxAge+';'
+    document.cookie = cookie;
+    parseCookie(cookie);
 }
-function getCookie(param, ele){
+
+function getCookie(param, ele){ //쿠키 가져오기
 
         if(JSON.parse(arr[param].isOn)){
-            console.log('shit')
             deleteCookie(param, -1);
             arr[param].btn_color = 'grey';
             arr[param].isOn = false;
@@ -86,12 +91,12 @@ function getCookie(param, ele){
             arr[param].btn_color = 'gold';
             arr[param].isOn = true;
             ele.style.backgroundColor = arr[param].btn_color;
-            setCookie(param,300);
+            setCookie(param,20);
         }
 }
 
 function deleteCookie(p, mxAge){
-    console.log(p, mxAge, `index${p}=`+JSON.stringify(arr[p]) + ";" + 'max-age='+mxAge+';');
     document.cookie = `index${p}=`+JSON.stringify(arr[p]) + ";" + 'max-age='+mxAge+';';
-    console.log(document.cookie);
+    cookies = (cookies.filter(obj => obj.index !== p ));
+
 }
