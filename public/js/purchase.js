c533382fffc24bb1e9efa169ca6e7ff4e915f561
chul soo <opportunity13@naver.com>
2023-01-17 16:14:07 +0900

    add cookie parse func

---
+++ public/js/purchase.js
@@ -0,0 +1,27 @@
+var cookies = [];
+
+pushCookie = (ck) => {
+    cookies.push(JSON.parse(ck));
+}
+
+if(document.cookie !== ''){
+    let cookie = document.cookie;
+    let array;
+    let on;
+    for(str of cookie){ //배열 길이를 지정하지않아 undefined가 들어가는 것을 방지
+        if(str == '{'){
+            array = '';
+            on = 1;
+        }
+        if(on){ 
+            if(str == '}'){
+                array += str;
+                on = 0;
+                pushCookie(array);
+            }else{
+                array += str;
+            }
+        }
+        
+    }
+}
